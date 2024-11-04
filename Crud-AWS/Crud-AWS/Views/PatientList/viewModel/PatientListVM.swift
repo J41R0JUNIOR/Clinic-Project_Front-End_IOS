7916d0e967e8b7ccc49fc133bbfe1a547f807d0b
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-11-03 21:58:09 -0300

    created a  filter for phone number

--- Crud-AWS/Crud-AWS/Views/PatientList/viewModel/PatientListVM.swift
+++ Crud-AWS/Crud-AWS/Views/PatientList/viewModel/PatientListVM.swift
@@ -12,6 +12,9 @@ class PatientListVM {
     var model = PatientListM()
     
      func loadPatients() async {
+         model.patientsLoaded.removeAll()
+         model.isLoading = true
+         
          Task{
              do {
                  if let patients: [Patient] = try await model.api.getAllData(urlString: URLs.getAllPatients.url){
@@ -24,4 +27,20 @@ class PatientListVM {
              }
          }
     }
+
+    func formatPhoneNumber(_ phoneNumber: String) -> String {
+        let digits = phoneNumber.filter { $0.isNumber }
+        
+        // Verifique se o número de dígitos é suficiente para a formatação
+        guard digits.count >= 10 else { return phoneNumber }
+
+        // Para números no formato: (XX) XXXXX-XXXX
+        let areaCode = digits.prefix(2)
+        let prefix = digits.dropFirst(2).prefix(5)
+        let lineNumber = digits.dropFirst(7).prefix(4)
+        
+        return "(\(areaCode)) \(prefix)-\(lineNumber)"
+    }
+
+
 }
