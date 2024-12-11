7ccb344c2bd5f252bcbc8c0433cdecfca8d69087
Jairo Júnior <jairojjr2003@gmail.com>
2024-12-11 11:08:22 -0300

    setup

---
+++ Clinic Project/Clinic Project/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Clinic Project
+//
+//  Created by The Godfather Júnior on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
